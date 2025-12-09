2dd6923c070ea506be222de700b2e04cfa7e10b2
--get <--get>
2025-12-09 20:13:47 +0800

    Update project configuration and permissions for Run Tracker app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bottom_sheet_demo"
+    namespace = "ph.francisco.run_tracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bottom_sheet_demo"
+        applicationId = "ph.francisco.run_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
